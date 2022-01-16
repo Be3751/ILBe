ed406a4c1fe0d1a7916206145697ce9dc2bd644b
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-01-16 21:41:14 +0900

    modified STATIC_ROOT

--- ILBe/settings.py
+++ ILBe/settings.py
@@ -147,14 +147,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # Local
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Heroku
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = (
-    (os.path.join(BASE_DIR, 'staticfiles')),
-)
+# STATICFILES_DIRS = (
+#     (os.path.join(BASE_DIR, 'staticfiles')),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
