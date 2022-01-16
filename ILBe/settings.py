e70f503d43982225d1dc86c259e03eeb8a048f3f
薗部匡矢 <msonobe@sonobetadashiyas-MacBook-Air.local>
2022-01-16 21:38:16 +0900

    modified STATIC_ROOT

--- ILBe/settings.py
+++ ILBe/settings.py
@@ -147,10 +147,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # Local
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # Heroku
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = (
     (os.path.join(BASE_DIR, 'staticfiles')),
